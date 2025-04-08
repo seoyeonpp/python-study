9f99a18fb27f457382ceae8c491241bfe93790af
seoyeonpp <seoyeonpub@gmail.com>
2025-04-08 11:26:49 +0900

    퀵 정렬 디버깅

--- algorithm/sort/sort_example.py
+++ algorithm/sort/sort_example.py
@@ -81,7 +81,7 @@ def merge_sort(arr):
     # 리스트를 반으로 나누고, 정렬해서 합치는 분할 정복 알고리즘
     # 평균,최악,최선 - O(n log n)
     # 리스트가 엄청 크고, 안정성이 필요할때 좋음.
-    if len(arr) <= 1:  # base case
+    if len(arr) <= 1:  # 기저 조건
         return arr
 
     mid = len(arr) // 2
@@ -95,16 +95,17 @@ def quick_sort(arr):
     # 최선,평균 - O(n log n), 최악 - O(n^2)
     # n 개의 데이터를 log n 깊이로 계속 나눔
 
-    if len(arr) <= 1:
+    if len(arr) <= 1: # 기저 조건
         return arr
     #pivot = arr[0] # 실행 시간: 0.000374초로 느림.. 0 번째가 가장 작은수거나 큰수가 되면 left나 right가 빈배열이 되어서 선형비교 수행해서 느림
     pivot = random.choice(arr) # pivot을 랜덤으로 선택 - 실행 시간: 0.000166초
-    tail = arr[1:]
+    print("pivot:", pivot)
 
-    left = [ x for x in tail if x <= pivot ]
-    right = [ x for x in tail if x > pivot ]
+    left = [x for x in arr if x < pivot] # pivot보다 작은 값들
+    mid = [x for x in arr if x == pivot]  # pivot과 같은 값들
+    right = [x for x in arr if x > pivot] # pivot보다 큰 값들
 
-    return quick_sort(left) + [pivot] + quick_sort(right)
+    return quick_sort(left) + mid + quick_sort(right)
 
 
 if __name__=="__main__":
@@ -115,4 +116,4 @@ if __name__=="__main__":
     selection_sort(random_array) # 실행 시간: 0.000022초
     insertion_sort(random_array) # 실행 시간: 0.000009초 - 왜 빠를까? : 버블, 선택정렬은 항상 전체 범위를 비교하지만 삽입 정렬은 앞쪽이 정렬되어있으면 바로 넘어감 (early stop)
     print("병합정렬 완료 ", merge_sort(random_array)) # 실행 시간: 0.000167초 - 왜 느릴까? : 데이터 양이 작을땐 단순한 정렬이 더 빨라서그렇다. 재귀 + 리스트복사가 많다.
-    print("퀵정렬 완료 ", quick_sort(random_array)) # 실행 시간: 0.000166초
+    print("퀵정렬 완료 ", quick_sort(random_array)) # 실행 시간: 0.000158초
