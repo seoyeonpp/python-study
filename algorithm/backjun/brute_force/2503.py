66efff3ff179d2392373f28a1dc471102f05bf63
seoyeonpp <seoyeonpub@gmail.com>
2025-09-29 23:12:47 +0900

    2503 푸는중

---
+++ algorithm/backjun/brute_force/2503.py
@@ -0,0 +1,14 @@
+# 백준 : https://www.acmicpc.net/problem/2503
+
+def answer(ans):
+    num, strike, ball = ans
+
+    return
+
+
+N = int(input())
+question_list = [list(map(int,input().split())) for _ in range(0,N)]
+
+init = [True,True,True]
+for item in range(0,N):
+    print(answer(item))
