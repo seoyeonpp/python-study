a887368b235b4d11b7b4f6e9b4fd2e6fea7c9717
seoyeonpp <seoyeonpub@gmail.com>
2025-10-21 23:21:11 +0900

    2503 풀었다

--- algorithm/backjun/brute_force/2503.py
+++ algorithm/backjun/brute_force/2503.py
@@ -1,14 +1,37 @@
 # 백준 : https://www.acmicpc.net/problem/2503
 
-def answer(ans):
-    num, strike, ball = ans
-
-    return
-
+from itertools import permutations
 
 N = int(input())
-question_list = [list(map(int,input().split())) for _ in range(0,N)]
+num_list = [input().split() for _ in range(0,N)]
 
-init = [True,True,True]
-for item in range(0,N):
-    print(answer(item))
+answer = 0
+# 0은 나오면 안되니까 1~9까지로 순열
+for i in permutations(range(1,10),3):
+    valid = True
+
+    for j in range(N):
+        target,strike,ball = num_list[j]
+        strike = int(strike)
+        ball = int(ball)
+        # temp 설정
+        tmp_strike = tmp_ball = 0
+
+        # 3자리수 비교
+        for k in range(3):
+            if i[k] == int(target[k]):
+                tmp_strike += 1
+            elif str(i[k]) in str(target):
+                tmp_ball += 1
+
+        # 하나라도 안같으면 종료
+        if tmp_strike != strike or tmp_ball != ball:
+            # N개중에 1개라도 틀렸으니까 이번 순서 숫자는 끝! 종료처리
+            valid = False
+            break
+    if valid:
+        # N개중에 모든 조건을 만족한 숫자는 답이 될 경우의 수에 추가
+        answer += 1
+print(answer)
+
+
