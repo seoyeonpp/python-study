c84be39f126ba34fa6834e70eac186bd1977f6ec
seoyeonpp <seoyeonpub@gmail.com>
2025-10-22 23:04:04 +0900

    조합

--- algorithm/backjun/brute_force/1182.py
+++ algorithm/backjun/brute_force/1182.py
@@ -1,39 +1,15 @@
 # 백준 https://www.acmicpc.net/problem/1182
 
+from itertools import combinations
+
 N, S = input().split()
 arr = list(map(int,input().split()))
 
 answer = 0
-size = 1
-pointer = 0
-check_arr = []
 
-
-def solve():
-    global answer
-    global size
-    global pointer
-    global check_arr
-
-    print(f"size: {size} / pointer : {pointer} / check_arr:  {check_arr}")
-
-    check_arr.append(arr[pointer])
-    pointer += 1
-
-    if len(check_arr) == size:
-        if sum(check_arr) == S:
+for count in range(1, int(N)+1):
+    for combo in combinations(arr,count):
+        if sum(combo) == int(S):
             answer += 1
-        else:
-            check_arr.pop()
-            solve()
 
-    elif pointer >= int(N):
-        pointer = 0
-        check_arr = []
-        size += 1
-
-
-solve()
 print(answer)
-
-
