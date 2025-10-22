be62b7422e24f28226454b97bec05c288f8a417c
seoyeonpp <seoyeonpub@gmail.com>
2025-10-22 19:01:51 +0900

    1182 푸는중

--- algorithm/backjun/brute_force/1182.py
+++ algorithm/backjun/brute_force/1182.py
@@ -4,10 +4,36 @@ N, S = input().split()
 arr = list(map(int,input().split()))
 
 answer = 0
-for size in range(1, int(N) + 1):
-    for start in range(int(N) - size + 1):
-        if sum(arr[start:start+size]) == int(S):
-            answer += 1
+size = 1
+pointer = 0
+check_arr = []
 
+
+def solve():
+    global answer
+    global size
+    global pointer
+    global check_arr
+
+    print(f"size: {size} / pointer : {pointer} / check_arr:  {check_arr}")
+
+    check_arr.append(arr[pointer])
+    pointer += 1
+
+    if len(check_arr) == size:
+        if sum(check_arr) == S:
+            answer += 1
+        else:
+            check_arr.pop()
+            solve()
+
+    elif pointer >= int(N):
+        pointer = 0
+        check_arr = []
+        size += 1
+
+
+solve()
 print(answer)
+
 
